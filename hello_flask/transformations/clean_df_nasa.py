7e9b5866f6509281d8477758dcdb0a40b73bc1ea
Max <maximiliano.vaca@uabc.edu.mx>
2024-10-06 10:45:02 -0700

    dropNA en clean_df_nasa.py 👷

--- hello_flask/transformations/clean_df_nasa.py
+++ hello_flask/transformations/clean_df_nasa.py
@@ -1,8 +1,11 @@
 import pandas as pd
 
 PATH = '/home/max/workspace/NASA_Spaceapps_2024/data/DEF_SRDB_V5_1827_5-20241005_212220/srdb-data-V5.csv'
-#RELATIVE_PATH = 'data/DEF_SRDB_V5_1827_5-20241005_212220/srdb-data-V5.csv'
-RELATIVE_PATH = 'hello_flask/data/DEF_SRDB_V5_1827_5-20241005_212220/srdb-data-V5.csv'
+REL_PATH = 'hello_flask/data/DEF_SRDB_V5_1827_5-20241005_212220/srdb-data-V5.csv'
+
+DATADIR_PATH = '/home/max/workspace/NASA_Spaceapps_2024/hello_flask/data'
+DATADIR_REL_PATH = 'hello_flask/data/Processed'
+
 
 to_keep = [
     'Entry_date', 
@@ -36,9 +39,24 @@ to_drop = [
     'Site_ID'
 ]
 
+# CONTIENE: 
+# FILTRA COLUMNAS, DROPNA,  Study_midyear 1ra, SORT, EXPORTA
 def create_dataframe_nasa():
-    df = pd.read_csv(RELATIVE_PATH, parse_dates=['Entry_date']).drop(columns=to_drop)
+    #df = pd.read_csv(RELATIVE_PATH, parse_dates=['Entry_date']).drop(columns=to_drop)
     #exporting the data
-    df.to_csv('clean_1_srdb-data-V5.csv', index=False)
+    #df.to_csv('clean_1_srdb-data-V5.csv', index=False)
+    
+    # Leer el archivo CSV y seleccionar solo las columnas especificadas                 # NA DROPPED
+    df = pd.read_csv(REL_PATH, usecols=to_keep, parse_dates=['Entry_date']).drop(columns=to_drop).dropna()
+
+    # Reorganizar las columnas para que 'Study_midyear' sea la primera
+    columns = ['Study_midyear'] + [col for col in df.columns if col != 'Study_midyear']
+    df = df[columns]
+
+    # Ordenar el DataFrame por 'Study_midyear'
+    df = df.sort_values(by='Study_midyear').reset_index(drop=True)
+    
+    # Exportar los datos
+    df.to_csv(DATADIR_REL_PATH+'/Processed/1_prod_clean_srdb-data-V5.csv', index=False)
     
     return df
